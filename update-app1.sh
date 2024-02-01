5fad96f58d3115ffdfab6bd0d4d285ca7c7c6722
GarryOne <igor.mardari96@gmail.com>
2024-02-01 13:31:09 +0100

    Added correct MONGO_USER and PASS in docker-compose.yaml
    Improved the kubernetes.yml deployment pipeline so that it does the ECR push

--- update-app1.sh
+++ update-app1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build -t 665357118005.dkr.ecr.eu-north-1.amazonaws.com/app1:v1 -f ./docker/Dockerfile-api .
-docker push 665357118005.dkr.ecr.eu-north-1.amazonaws.com/app1:v1
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 665357118005.dkr.ecr.eu-north-1.amazonaws.com
+
+docker build -t 665357118005.dkr.ecr.eu-north-1.amazonaws.com/app1:latest -f ./docker/Dockerfile-api .
+docker push 665357118005.dkr.ecr.eu-north-1.amazonaws.com/app1:latest
--- update-app2.sh
+++ update-app2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build -t 665357118005.dkr.ecr.eu-north-1.amazonaws.com/app2:v1 -f ./docker/Dockerfile-app .
-docker push 665357118005.dkr.ecr.eu-north-1.amazonaws.com/app2:v1
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 665357118005.dkr.ecr.eu-north-1.amazonaws.com
+
+docker build -t 665357118005.dkr.ecr.eu-north-1.amazonaws.com/app2:latest -f ./docker/Dockerfile-app .
+docker push 665357118005.dkr.ecr.eu-north-1.amazonaws.com/app2:latest
